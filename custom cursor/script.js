82a8856a55ac46dc128a257e65e0bfa6493076df
Roshan <155551955+okroshan4u@users.noreply.github.com>
2025-06-04 20:08:25 +0530

    Deleted custom cursor directory for update

--- custom cursor/script.js
+++
@@ -1,9 +0,0 @@
-const main = document.querySelector("#main")
-const cursor = document.querySelector(".cursor")
-
-main.addEventListener("mousemove",function(dets){
-    // cursor.style.left = (dets.x + 20 )+ "px" 
-    // cursor.style.top = (dets.y + 20) + "px"
-    cursor.style.left = (dets.x )+ "px" 
-    cursor.style.top = (dets.y) + "px"
-})
