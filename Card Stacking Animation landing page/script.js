e84c0eb8be1fd7abecf0f1b6a7f7f32ae326cabd
Roshan <155551955+okroshan4u@users.noreply.github.com>
2025-06-04 20:04:02 +0530

    uploaded the modern card stacking landing page

---
+++ Card Stacking Animation landing page/script.js
@@ -0,0 +1,61 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const scrollSection = document.querySelectorAll(".scroll-section");
+
+scrollSection.forEach((section) =>{
+    const wrapper = section.querySelector(".wrapper");
+    const items = wrapper.querySelectorAll(".item");
+
+    let direction = null;
+
+    if(section.classList.contains("vertical-section")){
+        direction = "vertical";
+    }else if(section.classList.contains("horizontal-section")){
+        direction = "horizontal"
+    }
+
+    initScroll(section, items,direction);
+});
+
+function initScroll(section, items, direction){
+    items.forEach((item,index)=>{
+        if(index !== 0){
+            direction == "horizontal"
+             ? gsap.set(item, {xPercent:100})
+             :gsap.set(item,{yPercent:100});
+        }
+    });
+
+    const timeline = gsap.timeline({
+        scrollTrigger:{
+            trigger:section,
+            pin:true,
+            start:"top top",
+            end:()=>  `+=${items.length * 100}%`,
+            scrub:1,
+            invalidateOnRefresh:true,
+        },
+        defaults:{ease:"none"},
+    });
+
+    items.forEach((item, index)=> {
+        timeline.to(item,{
+            scale:0.9,
+            borderRadius:"10px",
+
+        });
+
+        direction == "horizontal"?timeline.to(items [index + 1],
+            {
+                xPercent: 0,
+            },
+            "<"
+        ): timeline.to(
+            items[index + 1],
+            {
+                yPercent:0,
+            },
+            "<"
+        );
+    });
+}
